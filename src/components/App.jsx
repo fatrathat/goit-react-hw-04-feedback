ec99b2ec8c9c1f53546ec8cdb176145608be9ee7
Eugene <eugene.multan@gmail.com>
2022-07-22 13:37:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
